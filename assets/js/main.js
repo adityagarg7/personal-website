f45b481b794826d3f30edd502b506bfb5f7e4952
Aditya Garg <adityagarg.edu@gmail.com>
2020-08-17 04:11:18 +0530

    Reduce fade-in duration for home-page title

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ const throttle = (callback, limit) => {
   };
 };
 
-// Animation for home page title
+// Fade animation for home page title (code begins)
 
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml3');
@@ -27,9 +27,52 @@ anime.timeline({loop: false})
     targets: '.ml3 .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 2000,
+    duration: 1500,
     delay: (el, i) => 150 * (i+1)
   })
+// Fade animation for home page title (code ends)
+
+// //typeWriter animation for homepage title (code begins) (https://codepen.io/Danielgroen/pen/VeRPOq)
+// document.addEventListener('DOMContentLoaded',function(event){
+//   // array with texts to type in typewriter
+//   var dataText = [ "Developer.", "Data Enthusiast.", "Dog Lover.", "GARG"];
+//
+//   // type one text in the typwriter
+//   // keeps calling itself until the text is finished
+//   function typeWriter(text, i, fnCallback) {
+//     // check if text isn't finished yet
+//     if (i < (text.length)) {
+//       // add next character to h1
+//      document.querySelector("#home-title").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+//
+//       // wait for a while and call this function again for next character
+//       setTimeout(function() {
+//         typeWriter(text, i + 1, fnCallback)
+//       }, 100);
+//     }
+//     document.querySelector("#home-title").innerHTML = '<span aria-hidden="true"></span>';
+//     // text finished, call callback if there is a callback function
+//     else if (typeof fnCallback == 'function') {
+//       // call callback after timeout
+//       setTimeout(fnCallback, 1000);
+//     }
+//   }
+//   // start a typewriter animation for a text in the dataText array
+//    function StartTextAnimation(i) {
+//      // check if dataText[i] exists
+//     if (i < dataText[i].length) {
+//       // text exists! start typewriter animation
+//      typeWriter(dataText[i], 0, function(){
+//        // after callback (and whole text has been animated), start next text
+//        StartTextAnimation(i + 1);
+//      });
+//     }
+//   }
+//   // start the text animation
+//   StartTextAnimation(0);
+// });
+//
+// //typeWriter animation for homepage title (code ends)
 
 // addEventListener Helper
 //
